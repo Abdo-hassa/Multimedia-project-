cd37cc1adeb3590aa954b828a13f0a0344a4b85d
abdo <abdo45010@gmail.com>
2021-02-08 13:56:12 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace multiMedisa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open = new System.Windows.Forms.Button();
+            this.txt_data = new System.Windows.Forms.RichTextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.out_put = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.BackColor = System.Drawing.Color.GhostWhite;
+            this.open.Font = new System.Drawing.Font("Viner Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.open.Location = new System.Drawing.Point(423, 106);
+            this.open.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(176, 65);
+            this.open.TabIndex = 0;
+            this.open.Text = "Selecct File";
+            this.open.UseVisualStyleBackColor = false;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // txt_data
+            // 
+            this.txt_data.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_data.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.txt_data.Location = new System.Drawing.Point(12, 282);
+            this.txt_data.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_data.Name = "txt_data";
+            this.txt_data.Size = new System.Drawing.Size(313, 276);
+            this.txt_data.TabIndex = 1;
+            this.txt_data.Text = "";
+            this.txt_data.TextChanged += new System.EventHandler(this.txt_data_TextChanged);
+            // 
+            // convert
+            // 
+            this.convert.BackColor = System.Drawing.Color.GhostWhite;
+            this.convert.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convert.ForeColor = System.Drawing.Color.DeepPink;
+            this.convert.Location = new System.Drawing.Point(51, 131);
+            this.convert.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(317, 55);
+            this.convert.TabIndex = 2;
+            this.convert.Text = "convert RL";
+            this.convert.UseVisualStyleBackColor = false;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GhostWhite;
+            this.button1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DeepPink;
+            this.button1.Location = new System.Drawing.Point(51, 191);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "convert dictionary";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GhostWhite;
+            this.button2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DeepPink;
+            this.button2.Location = new System.Drawing.Point(51, 73);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "convert VL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // out_put
+            // 
+            this.out_put.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.out_put.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.out_put.Location = new System.Drawing.Point(331, 282);
+            this.out_put.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.out_put.Name = "out_put";
+            this.out_put.Size = new System.Drawing.Size(313, 276);
+            this.out_put.TabIndex = 5;
+            this.out_put.Text = "";
+            this.out_put.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label1.Location = new System.Drawing.Point(28, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label2.Location = new System.Drawing.Point(28, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label3.Location = new System.Drawing.Point(28, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DeepPink;
+            this.label4.Location = new System.Drawing.Point(336, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Compress Data";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DeepPink;
+            this.label5.Location = new System.Drawing.Point(25, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 26);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tempus Sans ITC", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(116, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(407, 37);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Algorithms For Cmopress Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(668, 564);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.out_put);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.txt_data);
+            this.Controls.Add(this.open);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.RichTextBox txt_data;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox out_put;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace multiMedisa
+{
+    public partial class Form1 : Form
+    {
+        public string s,s_out="";
+        string output = "";
+        string output1 = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd=new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                 s = File.ReadAllText(ofd.FileName);
+                txt_data.Text = s;
+            }
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            int c = 1;
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < s.Length - 1)
+                {
+                    if (s[i] == s[i + 1])
+                        c++;
+                    else
+                    {
+                        s_out += c;
+                        s_out += s[i];
+                        c = 1;
+                    }
+
+                }
+                else
+                {
+                    s_out += c;
+                    s_out += s[i];
+                }   
+                
+            }
+            File.WriteAllText(@"D:\RLC.txt",s_out);
+            out_put.Text = s_out;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> dic = new List<string>();
+            List<int> code = new List<int>();
+            dic.Add("a");
+            dic.Add("b");
+            dic.Add("c");
+            code.Add(1);
+            code.Add(2);
+            code.Add(3);
+            int count = 4;
+            string c ="";
+            string next = "";
+            
+            c = s[0].ToString();
+                              
+            for (int i = 0; i < s.Length ; i++) {
+                if (i < s.Length - 1)
+                {
+                    next = s[i + 1].ToString();
+                    if (!dic.Contains(c + next))
+                    {
+                        dic.Add(c + next);
+                        code.Add(count++);
+                        output += code.ElementAt(dic.IndexOf(c));
+                        c = next;
+                    }
+                    else
+                    {
+                        c = c + next;
+                    }
+                }
+                else
+                {
+                    output += code.ElementAt(dic.IndexOf(c));
+                }
+
+            }
+            File.WriteAllText(@"D:\Dictionary.txt", output);
+            out_put.Text = output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           char[]s1=s.ToArray();
+           Array.Sort(s1);
+           string s2="";
+           s2 = string.Join("", s1.Distinct());
+           List<string> vls = new List<string>();
+           List<string> vls1 = new List<string>();
+           List<int> vlc = new List<int>();
+           int c=1;
+           for (int i = 0; i < s.Length; i++)
+           {
+               if (i < s.Length - 1)
+               {
+                   if (s1[i] == s1[i + 1])
+                       c++;
+                   else
+                   {
+                       vls.Add(s1[i].ToString());
+
+                       vlc.Add(c);
+                       c = 1;
+                   }
+
+               }
+               else
+               {
+                   vls.Add(s1[i].ToString());
+                   vlc.Add(c); 
+               }
+           }
+           for (int i = 0; i < vlc.Count; i++)
+           {
+               int m= vlc.Max();
+               m = vlc.IndexOf(m);
+               vls1.Add(vls[m]);
+               vlc[m] = 0;
+           }
+           vls.Clear();
+           string str = "0";
+           string str2 = "";
+            for (int i = 0; i < vls1.Count; i++)
+           {
+               if (vls1.Count > i+1)
+               {
+                   vls.Add (str);
+                   str += "1";
+                   str2 += "1";
+               }
+               else
+               {
+                   vls.Add(str2);
+               }
+           }
+
+           for (int i = 0; i < s.Length; i++) {
+               int m=vls1.IndexOf(s[i].ToString());
+               output1 += vls[m];
+           }
+           File.WriteAllText(@"D:\VLC.txt", output1);
+           out_put.Text = output1;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_data_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
